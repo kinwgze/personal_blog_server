51772944609bc35973400a7532d525b6e5177993
zeeew <wz297443228@outlook.com>
2022-11-21 16:26:09 +0800

    2022/11/21 swagger config 【1】

--- src/main/java/zeee/blog/common/swagger/SwaggerConfig.java
+++ src/main/java/zeee/blog/common/swagger/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package zeee.blog.common.swagger;
 
+import io.swagger.annotations.Api;
+import io.swagger.models.Contact;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -22,15 +24,25 @@ public class SwaggerConfig{
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                // 扫描的包路径
-                .apis(RequestHandlerSelectors.any())
+                /* 扫描的包路径
+                    RequestHandlerSelectors.any()   为任何接口生成API文档
+                    RequestHandlerSelectors.none()  不生成任何接口文档
+                    RequestHandlerSelectors.basePackage("com.neutech.swagger2study")  只为这个包下生成接口文档
+                    RequestHandlerSelectors.withClassAnnotation(Api.class)  为有@Api注解的Controller生成API文档
+                    RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class) 为有@ApiOperation注解的方法生成API文档
+                 */
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                // PathSelectors.ant("/api/**") 过滤路径 只扫描/api/**的接口 生成接口文档
                 .paths(PathSelectors.any())
                 .build();
     }
 
+
+    //swagger信息
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("zeeew")
+                .title("Swagger中文文档")
+                .description("zeeew")
                 .version("1.0.0")
                 .build();
     }
