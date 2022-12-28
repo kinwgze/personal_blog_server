72ef07509be6dcb14e3d4b38e85471d2d9e62ad0
zeeew <wz297443228@outlook.com>
2022-12-28 20:32:01 +0800

    2022/12/28 weChat done

--- src/main/java/zeee/blog/wechat/task/WeChatTask.java
+++ src/main/java/zeee/blog/wechat/task/WeChatTask.java
@@ -3,6 +3,9 @@ package zeee.blog.wechat.task;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import zeee.blog.wechat.service.WeChatService;
+
+import javax.annotation.Resource;
 
 /**
  * @author wz
@@ -11,12 +14,14 @@ import org.springframework.stereotype.Component;
 @Component
 @EnableScheduling
 public class WeChatTask {
+    @Resource
+    private WeChatService weChatService;
     /**
      * 每天早上8点推送到微信
      */
     @Scheduled(cron = "0 0 8 ? * * ")
     public void pushMessage(){
-
+        weChatService.pushMessage();
     }
 
 }
